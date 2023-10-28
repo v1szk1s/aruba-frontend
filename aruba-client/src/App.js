7fef15628fc122f62fee3a4491a7a46cff95cca0
Kvak Barnabás <kvakbarnabas2003@gmail.com>
2023-10-28 15:54:10 +0200

    change: home

--- aruba-client/src/App.js
+++ aruba-client/src/App.js
@@ -4,17 +4,26 @@ import Home from "./pages/home/home";
 import Login from "./pages/login/login";
 import Register from "./pages/register/register";
 import Navbar from "./components/navbar/navbar";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
+    </ThemeProvider>
   );
 }
 
--- aruba-client/src/pages/home/home.js
+++ aruba-client/src/pages/home/home.js
@@ -5,9 +5,13 @@ import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import hiro from "../../img/hiro_icon.png";
+import RocketLaunchOutlinedIcon from "@mui/icons-material/RocketLaunchOutlined";
+import LanguageIcon from "@mui/icons-material/Language";
+import CloudDownloadIcon from "@mui/icons-material/CloudDownload";
+import Button from "@mui/material/Button";
 
 const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  backgroundColor: "#1A2027",
   ...theme.typography.body2,
   padding: theme.spacing(1),
   textAlign: "center",
@@ -19,10 +23,27 @@ function Home() {
     <>
       <Stack spacing={2}>
         <Item>
-          <Grid container direction="row" justifyContent="space-around">
+          <Grid
+            container
+            direction="row"
+            justifyContent="space-around"
+            alignItems="center"
+          >
             <Grid item xs={6}>
               <Item>
                 <h1>Deploy easily!</h1>
+              </Item>
+              <Item>
+                <RocketLaunchOutlinedIcon /> Limitless Possibilities
+              </Item>
+              <Item>
+                <LanguageIcon /> Universal Compatibilites
+              </Item>
+              <Item>
+                <CloudDownloadIcon /> Effortless Installation
+              </Item>
+              <Item>
+                <Button variant="contained">Register Now!</Button>
               </Item>
             </Grid>
             <Grid item xs={6}>
@@ -30,8 +51,46 @@ function Home() {
             </Grid>
           </Grid>
         </Item>
-        <Item>Item 2</Item>
+        <Item>
+          <Grid
+            container
+            direction="row"
+            justifyContent="space-around"
+            alignItems="center"
+          >
+            <Grid container xs={12}>
+              <Grid item>
+                <h1>We have 45,000 apps to deploy!</h1>
+              </Grid>
+
+              <Grid container spacing={2}>
+                <Grid item xs={6}>
+                  <Item>
+                    <p>
+                      Rest easy knowing that every extension featured on
+                      AruPlace
+                      <br></br>
+                      undergoes a stringent vetting process. Our commitment to
+                      security ensures that you can explore and experiment
+                      without compromising the integrity of your digital
+                      ecosystem.
+                    </p>
+                  </Item>
+                  <Item>
+                    <Button variant="contained">Register Now!</Button>
+                  </Item>
+                </Grid>
+                <Grid item xs={6}>
+                  <Item>
+                    <img src={hiro} />
+                  </Item>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Item>
         <Item>Item 3</Item>
+        <Item>Item 4</Item>
       </Stack>
     </>
   );
